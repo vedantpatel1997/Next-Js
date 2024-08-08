398efe2076913f7dd645109592f3911f1c70d1d9
vedantpatel1997 <vedantp9@gmail.com>
2024-08-07 22:49:45 -0600

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            By Vedant Patel{" "}
+            By Vedant Kalpesh Patel{" "}
             <Image
               src="/vercel.svg"
               alt="Vercel Logo"
